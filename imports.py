90dfae29dd8233c71f8ab061c56547ad81ab0276
Hrishikesh Pol <hrishikesh.pol1999@gmail.com>
2024-07-06 23:36:21 +0100

    Update imports.py

--- imports.py
+++ imports.py
@@ -10,28 +10,3 @@ try:
     import cPickle as pickle  # Conditional Import
 except ImportError:
     import pickle
-
-
-# Import styles breakdown:
-# Whole Module:
-#   - os: 1 times
-#   - sys: 1 times
-#   - pickle: 1 times
-# Specific Import:
-#   - os.path: 1 times
-#   - os.remove: 1 times
-# Wildcard Import:
-#   - os: 1 times
-# Alias Import:
-#   - numpy: 1 times
-#   - pandas: 1 times
-#   - matplotlib.pyplot: 1 times
-#   - cPickle: 1 times
-#   - Tkinter: 1 times
-#   - tkinter: 1 times
-# Conditional Import:
-#   - cPickle: 1 times
-# Relative Import:
-#   - .: 1 times
-# Relative Import Upwards:
-#   - None: 1 times
