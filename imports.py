7a4dc06649edcd376bc5c65de9be708e1af72815
Hrishikesh Pol <hrishikesh.pol1999@gmail.com>
2024-07-06 23:43:55 +0100

    Update imports.py

--- imports.py
+++ imports.py
@@ -1,12 +1,14 @@
 import os  # Whole Module Import
+import sys  # Whole Module Import
+
 from os import path  # Specific Import
 from os import *  # Wildcard Import
 import numpy as np  # Alias Import
-import sys  # Conditional Import (used in the try-except block)
-from . import sibling_module  # Relative Import
-from .. import parent_module  # Relative Import Upwards
 
 try:
     import cPickle as pickle  # Conditional Import
 except ImportError:
     import pickle
+
+from . import sibling_module  # Relative Import
+from .. import parent_module  # Relative Import Upwards
