56f76b0f0ede8875f05317ad6f8502c144f791e6
Hrishikesh Pol <hrishikesh.pol1999@gmail.com>
2024-07-16 02:14:34 +0100

    Update imports.py

--- imports.py
+++ imports.py
@@ -1,47 +1,47 @@
+# # import os  # Whole Module Import
+# # import sys  # Whole Module Import
+
+# # from os import path  # Specific Import
+# # from os import *  # Wildcard Import
+# # import numpy as np  # Alias Import
+
+# # try:
+# #     import cPickle as pickle  # Alias Import
+# # except ImportError:
+# #     import pickle  # Whole Module Import
+
+# # from . import sibling_module  # Relative Import
+# # from .. import parent_module  # Relative Import Upwards
+
+
 # import os  # Whole Module Import
 # import sys  # Whole Module Import
+# import numpy  # Whole Module Import
+# import pandas  # Whole Module Import
+# import matplotlib # Whole Module Import
 
-# from os import path  # Specific Import
+# from os import path, remove  # Specific Import
+# from os import sep, environ  # Specific Import
+# from numpy import array  # Specific Import (Third-party)
+# from pandas import DataFrame  # Specific Import (Third-party)
+
 # from os import *  # Wildcard Import
-# import numpy as np  # Alias Import
+# # from sys import *  # Wildcard Import
 
-# try:
-#     import cPickle as pickle  # Alias Import
-# except ImportError:
-#     import pickle  # Whole Module Import
+# from matplotlib import pyplot as plt  # Alias Import (Third-party)
+# from matplotlib import figure as Figure  # Alias Import  (Third-party)
+# import numpy as np  # Alias Import (Third-party)
+# import pandas as pd  # Alias Import (Third-party)
+# import matplotlib.pyplot as plt # Alias Import (Third-party)
+# import matplotlib.pyplot as plt # Alias Import (Third-party)
+# import seaborn as sns  # Alias Import (Third-party)
+# import matplotlib as mpl  # Alias Import (Third-party)
 
 # from . import sibling_module  # Relative Import
+# from . import another_sibling_module  # Relative Import
+
 # from .. import parent_module  # Relative Import Upwards
+# from .. import another_parent_module  # Relative Import Upwards
 
-
-import os  # Whole Module Import
-import sys  # Whole Module Import
-import numpy  # Whole Module Import
-import pandas  # Whole Module Import
-import matplotlib # Whole Module Import
-
-from os import path, remove  # Specific Import
-from os import sep, environ  # Specific Import
-from numpy import array  # Specific Import (Third-party)
-from pandas import DataFrame  # Specific Import (Third-party)
-
-from os import *  # Wildcard Import
-# from sys import *  # Wildcard Import
-
-from matplotlib import pyplot as plt  # Alias Import (Third-party)
-from matplotlib import figure as Figure  # Alias Import  (Third-party)
-import numpy as np  # Alias Import (Third-party)
-import pandas as pd  # Alias Import (Third-party)
-import matplotlib.pyplot as plt # Alias Import (Third-party)
-import matplotlib.pyplot as plt # Alias Import (Third-party)
-import seaborn as sns  # Alias Import (Third-party)
-import matplotlib as mpl  # Alias Import (Third-party)
-
-from . import sibling_module  # Relative Import
-from . import another_sibling_module  # Relative Import
-
-from .. import parent_module  # Relative Import Upwards
-from .. import another_parent_module  # Relative Import Upwards
-
-imports = [sys, pathlib]
-for i in imports: import i
+# imports = [sys, pathlib]
+# for i in imports: import i
