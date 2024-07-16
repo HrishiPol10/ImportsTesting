cf12679d484512c635ddaa009d3a9ab78684f954
Hrishikesh Pol <hrishikesh.pol1999@gmail.com>
2024-07-16 02:24:06 +0100

    Update specificFuntionsUsed.py

--- specificFuntionsUsed.py
+++ specificFuntionsUsed.py
@@ -1,10 +1,52 @@
 import os
 import sys
-from collections import defaultdict
+from collections import defaultdict, deque
 import numpy as np
+import pandas as pd
 
 def example_function():
-    np.array([1, 2, 3])
-    os.path('a', 'b')
-    os.path.join('a', 'b')
-    defaultdict(list)
+    # Single call
+    sys.exit(0)
+    
+    # Single call with attribute
+    path = os.path('a', 'b')
+    
+    # Double call
+    full_path = os.path.join('a', 'b')
+    
+    # Triple call
+    root, ext = os.path.splitext(os.path.basename(full_path))
+    
+    # Function from numpy
+    array = np.array([1, 2, 3])
+    
+    # Function from collections
+    d = defaultdict(list)
+    d['key'].append('value')
+    
+    # Function from collections with alias
+    q = deque([1, 2, 3])
+    q.appendleft(0)
+    
+    # Function from pandas
+    df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
+    
+    # Another function from numpy
+    mean = np.mean(array)
+    
+    # Another function from os.path
+    is_dir = os.path.isdir(full_path)
+
+    return {
+        "sys_exit": sys.exit,
+        "path": path,
+        "full_path": full_path,
+        "root": root,
+        "ext": ext,
+        "array": array,
+        "defaultdict": d,
+        "deque": q,
+        "dataframe": df,
+        "mean": mean,
+        "is_dir": is_dir
+    }
