230d949bf642a4c26adf4c0399a0f3638703ab6f
Hrishikesh Pol <hrishikesh.pol1999@gmail.com>
2024-07-16 02:28:16 +0100

    Update specificFuntionsUsed.py

--- specificFuntionsUsed.py
+++ specificFuntionsUsed.py
@@ -36,6 +36,68 @@ def example_function():
     
     # Another function from os.path
     is_dir = os.path.isdir(full_path)
+    # Single call
+    sys.exit(0)
+    
+    # Single call with attribute
+    path = os.path('a', 'b')
+    
+    # Double call
+    full_path = os.path.join('a', 'b')
+    
+    # Triple call
+    root, ext = os.path.splitext(os.path.basename(full_path))
+    
+    # Function from numpy
+    array = np.array([1, 2, 3])
+    
+    # Function from collections
+    d = defaultdict(list)
+    d['key'].append('value')
+    
+    # Function from collections with alias
+    q = deque([1, 2, 3])
+    q.appendleft(0)
+    
+    # Function from pandas
+    df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
+    
+    # Another function from numpy
+    mean = np.mean(array)
+    
+    # Another function from os.path
+    is_dir = os.path.isdir(full_path)
+    # Single call
+    sys.exit(0)
+    
+    # Single call with attribute
+    path = os.path('a', 'b')
+    
+    # Double call
+    full_path = os.path.join('a', 'b')
+    
+    # Triple call
+    root, ext = os.path.splitext(os.path.basename(full_path))
+    
+    # Function from numpy
+    array = np.array([1, 2, 3])
+    
+    # Function from collections
+    d = defaultdict(list)
+    d['key'].append('value')
+    
+    # Function from collections with alias
+    q = deque([1, 2, 3])
+    q.appendleft(0)
+    
+    # Function from pandas
+    df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
+    
+    # Another function from numpy
+    mean = np.mean(array)
+    
+    # Another function from os.path
+    is_dir = os.path.isdir(full_path)
 
     return {
         "sys_exit": sys.exit,
