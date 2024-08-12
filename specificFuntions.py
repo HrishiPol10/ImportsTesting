a4ca685ea812cfa58cebde2f812a0dd857e77649
Hrishikesh Pol <hrishikesh.pol1999@gmail.com>
2024-08-12 23:02:42 +0100

    Update specificFuntions.py

--- specificFuntions.py
+++ specificFuntions.py
@@ -1,17 +1,17 @@
-import os
-import sys
-import numpy as np
+# import os
+# import sys
+# import numpy as np
 
-sys.exit(0)
+# sys.exit(0)
 
-# Single call with attribute
-path = os.path('a', 'b')
+# # Single call with attribute
+# path = os.path('a', 'b')
 
-# Double call
-full_path = os.path.join('a', 'b')
+# # Double call
+# full_path = os.path.join('a', 'b')
 
-# Triple call
-root, ext = os.path.splitext(os.path.basename(full_path))
+# # Triple call
+# root, ext = os.path.splitext(os.path.basename(full_path))
 
-# Function from numpy
-array = np.array([1, 2, 3])
+# # Function from numpy
+# array = np.array([1, 2, 3])
