6fd268ba885e555cb26a638b44a925b8f13ee72e
Hrishikesh Pol <hrishikesh.pol1999@gmail.com>
2024-08-12 22:44:46 +0100

    Update imports.py

--- imports.py
+++ imports.py
@@ -1,58 +1,9 @@
-# # import os  # Whole Module Import
-# # import sys  # Whole Module Import
+import os  # Whole Module Import
 
-# # from os import path  # Specific Import
-# # from os import *  # Wildcard Import
-# # import numpy as np  # Alias Import
-
-# # try:
-# #     import cPickle as pickle  # Alias Import
-# # except ImportError:
-# #     import pickle  # Whole Module Import
-
-# # from . import sibling_module  # Relative Import
-# # from .. import parent_module  # Relative Import Upwards
-
-
-# import os  # Whole Module Import
-# import sys  # Whole Module Import
-# import numpy  # Whole Module Import
-# import pandas  # Whole Module Import
-# import matplotlib # Whole Module Import
-
-# from os import path, remove  # Specific Import
-# from os import sep, environ  # Specific Import
 from numpy import array  # Specific Import (Third-party)
-from pandas import DataFrame  # Specific Import (Third-party)
 
-from os import *  # Wildcard Import
+import seaborn as sns  # Alias Import (Third-party)
+
 from sys import *  # Wildcard Import
 
-from matplotlib import pyplot as plt  # Alias Import (Third-party)
-from matplotlib import figure as Figure  # Alias Import  (Third-party)
-import numpy as np  # Alias Import (Third-party)
-import pandas as pd  # Alias Import (Third-party)
-import matplotlib.pyplot as plt # Alias Import (Third-party)
-import matplotlib.pyplot as plt # Alias Import (Third-party)
-import seaborn as sns  # Alias Import (Third-party)
-import matplotlib as mpl  # Alias Import (Third-party)
-
 from . import sibling_module  # Relative Import
-from . import another_sibling_module  # Relative Import
-
-from .. import parent_module  # Relative Import Upwards
-from .. import another_parent_module  # Relative Import Upwards
-
-sys.exit(0)
-    
-# Single call with attribute
-path = os.path('a', 'b')
-    print(abc)
-# Double call
-full_path = os.path.join('a', 'b')
-    
-# Triple call
-root, ext = os.path.splitext(os.path.basename(full_path))
-
-# imports = [sys, pathlib]
-# for i in imports: import i
